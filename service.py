e9de3b2bf97d0284f6706b6fc6febc973541f70a
azzy9 <19675382+azzy9@users.noreply.github.com>
2023-08-09 10:18:30 +0100

    Clean Service

--- service.py
+++ service.py
@@ -17,19 +17,26 @@
 
 import xbmc
 import time
-from commoncore import kodi
 
-enable_updates = kodi.get_setting('enable_updates') == 'true'
+from commoncore.kodi import addon, logger
+
+enable_updates = addon.get_setting('enable_updates') == 'true'
 
 class UpdateService():
+
     last_run = False
-    update_interval = 86400    # every 24 hours
+    # every 24 hours
+    update_interval = 86400
+
     def update(self):
         if enable_updates:
             if not self.last_run or time.time() - self.last_run > self.update_interval:
                 self.last_run = time.time()
-                plugin_url = kodi.build_plugin_url({"mode": "update_addons", "quiet": "quiet"}, kodi.get_id())
-                kodi.execute_url(plugin_url)
+                plugin_url = addon.build_plugin_url({
+                    "mode": "update_addons",
+                    "quiet": "quiet"
+                }, addon.get_id())
+                addon.execute_url(plugin_url)
 
     def start(self):
 
@@ -37,9 +44,9 @@ class UpdateService():
         class Monitor(xbmc.Monitor):
             def onSettingsChanged(self):
                 global enable_updates
-                enable_updates = kodi.get_setting('enable_updates') == 'true'
+                enable_updates = addon.get_setting('enable_updates') == 'true'
         monitor = Monitor()
-        kodi.log("Service Starting...")
+        logger.log("Service Starting...")
 
         while not monitor.abortRequested():
             if monitor.waitForAbort(10):
@@ -50,7 +57,7 @@ class UpdateService():
 
 
     def shutdown(self):
-        kodi.log("Service Stopping...")
+        logger.log("Service Stopping...")
 
 
 if __name__ == '__main__':
